e0f505072be4ba05a85dffce0df5a0d518195353
Dulneth Santhuka <dulneth.sa@gmail.com>
2025-09-06 12:46:02 +0530

    Sample text on main app to test the project

--- Job-nest/App.js
+++ Job-nest/App.js
@@ -1,10 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Sample Text for testing purpose </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#afd7ffff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
