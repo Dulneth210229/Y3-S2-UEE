d7d95b8b15a9eb7ada35c3de1daae41c4ce16038
Dulneth Santhuka <dulneth.sa@gmail.com>
2025-09-06 13:58:40 +0530

    some styling

--- Job-nest/App.js
+++ Job-nest/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Sample Text for testing purpose </Text>
+      <Text style={styles.sampleText}>Sample Text for testing purpose </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +16,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#afd7ffff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  sampleText: {
+    fontSize: 22,
+    fontWeight: "bold",
   },
 });
