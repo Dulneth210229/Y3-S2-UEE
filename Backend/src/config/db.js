eccd387703ada81f6219e3e471229cc4339d1c3e
Dilmi Sansala <dilmisansala21@gmail.com>
2025-09-23 18:27:00 +0530

    expo setup

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB connected ....");
+    console.log("MongoDB connected👍...");
   } catch (error) {
     console.log("Error connecting to database", error);
     process.exit(1); // exit with failure
